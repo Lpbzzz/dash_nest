e679e988b539d9ce50235d7cb693d8cf99108f0a
lpb <462570646@qq.com>
2022-11-24 01:40:34 +0800

    add auth jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../user/entities/user.entity';
+
+const jwtModule = JwtModule.register({
+  secret: 'lpb',
+  signOptions: { expiresIn: '7d' },
+});
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [jwtModule, TypeOrmModule.forFeature([User])],
+  exports: [jwtModule],
 })
 export class AuthModule {}
-
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../user/entities/user.entity';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  //
+  createToken(user: Partial<User>) {
+    return this.jwtService.sign(user);
+  }
+
+  async login(loginUser: CreateUserDto) {
+    const { username, password } = loginUser;
+    const existUser = await this.userRepository.findOne({
+      where: { username },
+    });
+    if (!existUser) {
+      throw new HttpException('用户不存在', HttpStatus.BAD_REQUEST);
+    }
+    const isPasswordValid = await bcrypt.compareSync(
+      password,
+      existUser.password,
+    );
+    if (!isPasswordValid) {
+      throw new HttpException('密码错误', HttpStatus.BAD_REQUEST);
+    } else {
+      return {
+        token: this.createToken({
+          username,
+          id: existUser.id,
+          role: existUser.role,
+        }),
+        ...existUser,
+      };
+    }
+  }
+}
