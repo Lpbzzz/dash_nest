b8097e4de3b0f82697f5a4540f3d7a6b63a89850
lpb <462570646@qq.com>
2022-11-23 14:34:45 +0800

    init before env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
+  //http请求处理
   controllers: [AppController],
+  //注入器实例化的提供者
   providers: [AppService],
 })
 export class AppModule {}
